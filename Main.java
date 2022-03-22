4beb0e7195c1d28af7200e253580e7d00c788207
KEacper <101186813+KEacper@users.noreply.github.com>
2022-03-22 12:21:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+
+    boolean a = true;
+    double b = 2.3;
+    int c = 1;
+    long d = 32321;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  
+  }
+}
